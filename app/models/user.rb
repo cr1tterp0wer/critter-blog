278d50d2ff391042001202fe97edee8ee6a42e1d
cr1tterp0wer <capodacac@gmail.com>
2019-11-15 05:04:48 -0600

    feature/user: User model + Controller + view

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+  validates :username, uniqueness: true, presence:true
+  validates :password, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resource :contact_message, only: [:create, :destroy]
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root "static#home"
 
+  resource :contact_message, only: [:create, :destroy]
+  resources :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/:url" => "static#show"
 end
