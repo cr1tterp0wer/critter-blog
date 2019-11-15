0260d0ed492f2f4314f3af5aa27ec2dc4e8193af
cr1tterp0wer <capodacac@gmail.com>
2019-11-15 03:42:00 -0600

    feature/contact: user models

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  has_secure_password
+end
