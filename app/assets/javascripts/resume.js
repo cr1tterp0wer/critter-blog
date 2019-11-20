d7da3f9d18ee82a371bce2da949dd1d60b6ecea8
cr1tterp0wer <capodacac@gmail.com>
2019-11-20 06:59:26 -0600

    scroll on blog

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -3,16 +3,16 @@ document.addEventListener("turbolinks:load",function(){
    // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(e) {
     e.preventDefault();
-    if (location.pathname.replace(/^\//, '') == "" ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top)
+      }, 1000, "easeInOutExpo");
+      return false;
     }
+
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
